6756ece7a9d041bef60f0ff0facb642ef7ea9d82
Leandro Ribeiro Ferreira <leandro.r.ferreira@accenture.com>
2020-03-11 16:34:43 -0300

    Add project files.

---
+++ TestPDF/Program.cs
@@ -0,0 +1,46 @@
+﻿using PdfSharpCore.Pdf;
+using PdfSharpCore.Pdf.AcroForms;
+using PdfSharpCore.Pdf.IO;
+using System;
+
+namespace TestPDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Templates_POC\template.pdf";
+
+            var doc = PdfReader.Open(path, PdfDocumentOpenMode.Modify);
+
+            if(doc.AcroForm == null)
+            {
+                Console.WriteLine("Erro ao ler os campos do PDF!");
+                return ;
+            }
+
+            PdfAcroForm form = doc.AcroForm;
+
+            if (form.Elements.ContainsKey("/NeedAppearances"))
+            {
+                form.Elements["/NeedAppearances"] = new PdfBoolean(true);
+            }
+            else
+            {
+                form.Elements.Add("/NeedAppearances", new PdfBoolean(true));
+            }
+
+            PdfTextField merchantPorfileOwnerField = (PdfTextField)(form.Fields["merchant_profile_owners_email_2"]);
+            PdfCheckBoxField checkField = (PdfCheckBoxField)(form.Fields["seasson_Mar"]);
+                    
+            merchantPorfileOwnerField.ReadOnly = false;
+            checkField.ReadOnly = false;
+            checkField.Checked = true;
+            merchantPorfileOwnerField.Value = new PdfString("TEST ON C#");
+
+            doc.Save(@"C:\Templates_POC\newcopy.pdf");
+                       
+            Console.WriteLine("Hello World! " + merchantPorfileOwnerField.Text);
+        }
+    }
+}
